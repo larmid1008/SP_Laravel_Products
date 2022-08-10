7943a448a9040ff6076b0bfaef8537fc3d28da00
Sergey P <larmid1008@gmail.com>
2022-08-10 14:32:59 +0300

    Products model, migration, factory. Model, migration for product categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,6 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          Category::factory(20)->create();
+         Product::factory(20)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
